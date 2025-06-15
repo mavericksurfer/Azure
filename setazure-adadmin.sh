13e7941d5856a2cdaedb061335f18a327c82cf18
mavericksurfer <40652437+mavericksurfer@users.noreply.github.com>
2025-06-15 14:41:45 +1000

    Delete setazure-adadmin.sh

--- setazure-adadmin.sh
+++
@@ -1,4 +0,0 @@
-adadmin=$(az sql server ad-admin list --resource-group $SQLResourceGroup --server-name $SQLServerName --output tsv)
-if [ -z $adadmin ]; then
-  az sql server ad-admin create --resource-group $SQLResourceGroup --server-name $SQLServerName --display-name "$AADAdminGroupName" --object-id $AADAdminGroupID;
-fi
